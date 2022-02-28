b3a0a0909710abde39d1fd9e688a11020ab63864
Lisa Zacarias <zacarias@slac.stanford.edu>
2022-02-28 15:24:16 -0800

    before adding in high limits

--- alarm.py
+++ alarm.py
@@ -1,11 +1,27 @@
 from dataclasses import dataclass
-from typing import List
+from typing import List, Optional
 
 from cavityWidget import CavityWidget
 from epics import PV
 from pydm import Display
 from scLinac import Cryomodule, Linac, LINACS, CM_LINAC_MAP
-from CavityDisplay import statusCallback, severityCallback, changeShape
+from pydm.widgets.template_repeater import PyDMTemplateRepeater
+from frontEnd_constants import shapeParameterDict
+
+
+# Change PyDMDrawingPolygon color
+def changeShape(cavity_widget, shapeParameterObject):
+    cavity_widget.brush.setColor(shapeParameterObject.fillColor)
+    cavity_widget.penColor = shapeParameterObject.borderColor
+    cavity_widget.numberOfPoints = shapeParameterObject.numPoints
+    cavity_widget.rotation = shapeParameterObject.rotation
+
+
+# Updates shape depending on pv value
+def updateWidget(cavity_widget, value):
+    changeShape(cavity_widget,
+                shapeParameterDict[value] if value in shapeParameterDict
+                else shapeParameterDict[3])
 
 
 @dataclass()
@@ -27,20 +43,44 @@ class AlarmPV:
 
 
 class AlarmLinac(Linac):
-    def __init__(self, name, cryomoduleStringList):
-        super().__init__(name, cryomoduleStringList, cryomoduleClass=AlarmCryomodule)
+    def __init__(self, linacName, cryomoduleStringList):
+        print(cryomoduleStringList)
+        super().__init__(linacName, cryomoduleStringList, cryomoduleClass=AlarmCryomodule)
 
-        beamLineVacuumPVString = "VGXX:{linac}:0202:COMBO_P".format(linac=name)
+        self.widget: Optional[PyDMTemplateRepeater] = None
+
+        beamLineVacuumPVString = "VGXX:{linac}:0202:COMBO_P".format(linac=linacName)
         self.beamLineVacuumPV = AlarmPV(pv=PV(beamLineVacuumPVString),
                                         lowAlarmLimit=-2,
                                         lowWarningLimit=-1,
                                         highAlarmLimit=2,
                                         highWarningLimit=1)
 
+    @property
+    def isAlarming(self):
+        return self.beamLineVacuumPV.isAlarming
+
+    @property
+    def isWarning(self):
+        return self.beamLineVacuumPV.isWarning
+
+    def update(self):
+        if self.isAlarming:
+            self.widget.setStyleSheet("background-color: rgb(150, 0, 0);")
+
+        elif self.isWarning:
+            self.widget.setStyleSheet("background-color: rgb(244,230,67);")
+
+        else:
+            self.widget.setStyleSheet("")
+
 
 class AlarmCryomodule(Cryomodule):
     def __init__(self, cryoName, linacObject: AlarmLinac):
         super().__init__(cryoName, linacObject)
+
+        self.widget: Optional[CavityWidget] = None
+
         couplerVacuumPVString = "VGXX:{linac}:{cryomodule}14:COMBO_P".format(linac=linacObject.name,
                                                                              cryomodule=cryoName)
         self.couplerVacuumPV = AlarmPV(pv=PV(couplerVacuumPVString),
@@ -76,10 +116,28 @@ class AlarmCryomodule(Cryomodule):
                 or self.insulatingCouplerVacuumPV.isAlarming
                 or self.couplerVacuumPV.isAlarming)
 
+    def update(self):
+        if self.isAlarming:
+            updateWidget(self.widget, 2)
+
+        elif self.isWarning:
+            updateWidget(self.widget, 1)
+
+        else:
+            updateWidget(self.widget, 0)
+
 
 ALARM_LINAC_OBJECTS = []
 for idx, (name, cryomoduleList) in enumerate(LINACS):
     ALARM_LINAC_OBJECTS.append(AlarmLinac(name, cryomoduleList))
+
+
+def run():
+    while True:
+        for linac in ALARM_LINAC_OBJECTS:
+            linac.update()
+            for cryomodule in linac.cryomodules:
+                cryomodule.update()
 
 
 class CryoAlarmDisplay(Display):
@@ -115,20 +173,30 @@ class CryoAlarmDisplay(Display):
                                            highAlarmLimit=2,
                                            highWarningLimit=1)
 
-        cryomodules: List[CavityWidget] = self.ui.cryomodules.findChildren(CavityWidget)
-        for cryomoduleDisplayObj in cryomodules:
-            cryomoduleName = cryomoduleDisplayObj.cavityText
-            linacIdx = CM_LINAC_MAP[cryomoduleName]
-            linacObject = ALARM_LINAC_OBJECTS[linacIdx]
-            cryomoduleObject = linacObject.cryomodules[cryomoduleName]
+        templateRepeaters: List[PyDMTemplateRepeater] = [self.ui.L0B,
+                                                         self.ui.L1B,
+                                                         self.ui.L2B,
+                                                         self.ui.L3B]
+
+        for linacIdx, templateRepeater in enumerate(templateRepeaters):
+
+            cryomodules: List[CavityWidget] = templateRepeater.findChildren(CavityWidget)
+            for cryomoduleDisplayObj in cryomodules:
+                cryomoduleName = cryomoduleDisplayObj.cavityText
+                linacObject = ALARM_LINAC_OBJECTS[linacIdx]
+                linacObject.widget = templateRepeater
+                cryomoduleObject: AlarmCryomodule = linacObject.cryomodules[cryomoduleName]
+                cryomoduleObject.widget = cryomoduleDisplayObj
+
+        run()
 
 
 def alarmCallback(cryomodule: AlarmCryomodule, cavityWidget: CavityWidget, **kw):
     if cryomodule.isAlarming:
-        severityCallback(cavityWidget, 2)
+        updateWidget(cavityWidget, 2)
 
     elif cryomodule.isWarning:
-        severityCallback(cavityWidget, 1)
+        updateWidget(cavityWidget, 1)
 
     else:
-        severityCallback(cavityWidget, 0)
+        updateWidget(cavityWidget, 0)
