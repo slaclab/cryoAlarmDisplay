d7e597a5df7a3562e16d85848b0cd506366976f4
Lisa Zacarias <zacarias@slac.stanford.edu>
2022-02-28 18:04:52 -0800

    added callbacks and tested with simulacrum. Need to change shape borders so that L0B faulting is more obvious

--- alarm.py
+++ alarm.py
@@ -1,12 +1,15 @@
 from dataclasses import dataclass
-from typing import List, Optional
+from typing import List, Optional, Callable
 
 from cavityWidget import CavityWidget
 from epics import PV
 from pydm import Display
-from scLinac import Cryomodule, Linac, LINACS, CM_LINAC_MAP
+from scLinac import Cryomodule, Linac, LINACS, CM_LINAC_MAP, Cavity
 from pydm.widgets.template_repeater import PyDMTemplateRepeater
 from frontEnd_constants import shapeParameterDict
+from functools import partial
+
+TRANSFER_LINE_ALARM_LIMIT = 8E-5
 
 
 # Change PyDMDrawingPolygon color
@@ -27,56 +30,48 @@ def updateWidget(cavity_widget, value):
 @dataclass()
 class AlarmPV:
     pv: PV
-    lowAlarmLimit: float
-    lowWarningLimit: float
-    highAlarmLimit: float
-    highWarningLimit: float
+    alarmLimit: float
+    updateFunction: Callable
 
-    @property
-    def isWarning(self):
-        return (self.lowWarningLimit >= self.pv.value >= self.lowAlarmLimit
-                or self.highWarningLimit <= self.pv.value <= self.highAlarmLimit)
+    def __post_init__(self):
+        self.pv.add_callback(partial(alarmCallback, self.alarmLimit,
+                                     self.updateFunction))
 
-    @property
-    def isAlarming(self):
-        return self.pv.value >= self.highAlarmLimit or self.pv.value <= self.lowAlarmLimit
+    # @property
+    # def isAlarming(self):
+    #     return self.pv.value is None or self.pv.value >= self.alarmLimit
+
+
+def alarmCallback(alarmLimit, updateFunction, value, **kw):
+    isAlarming = value is None or value >= alarmLimit
+    updateFunction(isAlarming)
 
 
 class AlarmLinac(Linac):
     def __init__(self, linacName, cryomoduleStringList):
-        print(cryomoduleStringList)
-        super().__init__(linacName, cryomoduleStringList, cryomoduleClass=AlarmCryomodule)
+        super().__init__(linacName, cryomoduleStringList,
+                         cryomoduleClass=AlarmCryomodule)
 
         self.widget: Optional[PyDMTemplateRepeater] = None
 
         beamLineVacuumPVString = "VGXX:{linac}:0202:COMBO_P".format(linac=linacName)
         self.beamLineVacuumPV = AlarmPV(pv=PV(beamLineVacuumPVString),
-                                        lowAlarmLimit=-2,
-                                        lowWarningLimit=-1,
-                                        highAlarmLimit=2,
-                                        highWarningLimit=1)
+                                        alarmLimit=1.5E-8, updateFunction=self.update)
 
-    @property
-    def isAlarming(self):
-        return self.beamLineVacuumPV.isAlarming
+    # @property
+    # def isAlarming(self):
+    #     return self.beamLineVacuumPV.isAlarming
 
-    @property
-    def isWarning(self):
-        return self.beamLineVacuumPV.isWarning
-
-    def update(self):
-        if self.isAlarming:
+    def update(self, isAlarming):
+        if isAlarming:
             self.widget.setStyleSheet("background-color: rgb(150, 0, 0);")
-
-        elif self.isWarning:
-            self.widget.setStyleSheet("background-color: rgb(244,230,67);")
 
         else:
             self.widget.setStyleSheet("")
 
 
 class AlarmCryomodule(Cryomodule):
-    def __init__(self, cryoName, linacObject: AlarmLinac):
+    def __init__(self, cryoName, linacObject: AlarmLinac, cavityClass=Cavity):
         super().__init__(cryoName, linacObject)
 
         self.widget: Optional[CavityWidget] = None
@@ -84,44 +79,29 @@ class AlarmCryomodule(Cryomodule):
         couplerVacuumPVString = "VGXX:{linac}:{cryomodule}14:COMBO_P".format(linac=linacObject.name,
                                                                              cryomodule=cryoName)
         self.couplerVacuumPV = AlarmPV(pv=PV(couplerVacuumPVString),
-                                       lowAlarmLimit=-2,
-                                       lowWarningLimit=-1,
-                                       highAlarmLimit=2,
-                                       highWarningLimit=1)
+                                       alarmLimit=1E-5,
+                                       updateFunction=self.update)
 
         insulatingVacuumPVString = "VGXX:{linac}:{cryomodule}96:COMBO_P".format(linac=linacObject.name,
                                                                                 cryomodule=cryoName)
         self.insulatingCouplerVacuumPV = AlarmPV(pv=PV(insulatingVacuumPVString),
-                                                 lowAlarmLimit=-2,
-                                                 lowWarningLimit=-1,
-                                                 highAlarmLimit=2,
-                                                 highWarningLimit=1)
+                                                 alarmLimit=8E-5,
+                                                 updateFunction=self.update)
 
         lineBPressurePVString = "CPT:CM{cm}:2602:US:PRESS".format(cm=cryoName)
         self.lineBPressurePV = AlarmPV(pv=PV(lineBPressurePVString),
-                                       lowAlarmLimit=-2,
-                                       lowWarningLimit=-1,
-                                       highAlarmLimit=2,
-                                       highWarningLimit=1)
+                                       alarmLimit=2,
+                                       updateFunction=self.update)
 
-    @property
-    def isWarning(self):
-        return (self.lineBPressurePV.isWarning
-                or self.insulatingCouplerVacuumPV.isWarning
-                or self.couplerVacuumPV.isWarning)
+    # @property
+    # def isAlarming(self):
+    #     return (self.lineBPressurePV.isAlarming
+    #             or self.insulatingCouplerVacuumPV.isAlarming
+    #             or self.couplerVacuumPV.isAlarming)
 
-    @property
-    def isAlarming(self):
-        return (self.lineBPressurePV.isAlarming
-                or self.insulatingCouplerVacuumPV.isAlarming
-                or self.couplerVacuumPV.isAlarming)
-
-    def update(self):
-        if self.isAlarming:
+    def update(self, isAlarming):
+        if isAlarming:
             updateWidget(self.widget, 2)
-
-        elif self.isWarning:
-            updateWidget(self.widget, 1)
 
         else:
             updateWidget(self.widget, 0)
@@ -132,14 +112,6 @@ for idx, (name, cryomoduleList) in enumerate(LINACS):
     ALARM_LINAC_OBJECTS.append(AlarmLinac(name, cryomoduleList))
 
 
-def run():
-    while True:
-        for linac in ALARM_LINAC_OBJECTS:
-            linac.update()
-            for cryomodule in linac.cryomodules:
-                cryomodule.update()
-
-
 class CryoAlarmDisplay(Display):
     # def ui_filename(self):
     #     return "alarmDisplay.ui"
@@ -147,31 +119,18 @@ class CryoAlarmDisplay(Display):
     def __init__(self, parent=None, args=None, ui_filename="alarmDisplay.ui"):
         super(CryoAlarmDisplay, self).__init__(parent=parent, args=args,
                                                ui_filename=ui_filename)
-        self.ui.cryomodules.loadWhenShown = False
 
-        self.transferLineVacuum1 = AlarmPV(pv=PV("CPT:FC01:950:VG:PRESS"),
-                                           lowAlarmLimit=-2,
-                                           lowWarningLimit=-1,
-                                           highAlarmLimit=2,
-                                           highWarningLimit=1)
-
-        self.transferLineVacuum2 = AlarmPV(pv=PV("CPT:FC02:953:VG:PRESS"),
-                                           lowAlarmLimit=-2,
-                                           lowWarningLimit=-1,
-                                           highAlarmLimit=2,
-                                           highWarningLimit=1)
-
-        self.transferLineVacuum3 = AlarmPV(pv=PV("CPT:FC03:954:VG:PRESS"),
-                                           lowAlarmLimit=-2,
-                                           lowWarningLimit=-1,
-                                           highAlarmLimit=2,
-                                           highWarningLimit=1)
-
-        self.transferLineVacuum4 = AlarmPV(pv=PV("CPT:FC04:957:VG:PRESS"),
-                                           lowAlarmLimit=-2,
-                                           lowWarningLimit=-1,
-                                           highAlarmLimit=2,
-                                           highWarningLimit=1)
+        # self.transferLineVacuum1 = AlarmPV(pv=PV("CPT:FC01:950:VG:PRESS"),
+        #                                    alarmLimit=TRANSFER_LINE_ALARM_LIMIT)
+        #
+        # self.transferLineVacuum2 = AlarmPV(pv=PV("CPT:FC02:953:VG:PRESS"),
+        #                                    alarmLimit=TRANSFER_LINE_ALARM_LIMIT)
+        #
+        # self.transferLineVacuum3 = AlarmPV(pv=PV("CPT:FC03:954:VG:PRESS"),
+        #                                    alarmLimit=TRANSFER_LINE_ALARM_LIMIT)
+        #
+        # self.transferLineVacuum4 = AlarmPV(pv=PV("CPT:FC04:957:VG:PRESS"),
+        #                                    alarmLimit=TRANSFER_LINE_ALARM_LIMIT)
 
         templateRepeaters: List[PyDMTemplateRepeater] = [self.ui.L0B,
                                                          self.ui.L1B,
@@ -179,24 +138,13 @@ class CryoAlarmDisplay(Display):
                                                          self.ui.L3B]
 
         for linacIdx, templateRepeater in enumerate(templateRepeaters):
+            templateRepeater.loadWhenShown = False
 
             cryomodules: List[CavityWidget] = templateRepeater.findChildren(CavityWidget)
             for cryomoduleDisplayObj in cryomodules:
                 cryomoduleName = cryomoduleDisplayObj.cavityText
                 linacObject = ALARM_LINAC_OBJECTS[linacIdx]
                 linacObject.widget = templateRepeater
+                print([linacObject.name, linacObject.widget])
                 cryomoduleObject: AlarmCryomodule = linacObject.cryomodules[cryomoduleName]
                 cryomoduleObject.widget = cryomoduleDisplayObj
-
-        run()
-
-
-def alarmCallback(cryomodule: AlarmCryomodule, cavityWidget: CavityWidget, **kw):
-    if cryomodule.isAlarming:
-        updateWidget(cavityWidget, 2)
-
-    elif cryomodule.isWarning:
-        updateWidget(cavityWidget, 1)
-
-    else:
-        updateWidget(cavityWidget, 0)
